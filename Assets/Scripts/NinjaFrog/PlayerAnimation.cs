8878c334eb14418dac2747afe84dcd0a9867f5d0
Phaninder <phaninder2008@gmail.com>
2024-12-19 21:05:21 +0530

    Added jump animation

--- Assets/Scripts/NinjaFrog/PlayerAnimation.cs
+++ Assets/Scripts/NinjaFrog/PlayerAnimation.cs
@@ -6,11 +6,20 @@ public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField]
     private Animator animator;
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+    [SerializeField]
+    private float force;
+
+    [SerializeField]
+    private bool isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        isGrounded = true;
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,6 +29,8 @@ public class PlayerAnimation : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
+                var newScale = new Vector3(1, 1, 1);
+                transform.localScale = newScale;
                 animator.SetBool("Run", true);
             }
             else if (Input.GetKeyUp(KeyCode.RightArrow))
@@ -27,6 +38,45 @@ public class PlayerAnimation : MonoBehaviour
                 animator.SetBool("Run", false);
             }
 
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                var newScale = new Vector3(-1, 1, 1);
+                transform.localScale = newScale;
+                animator.SetBool("Run", true);
+            }
+            else if(Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                animator.SetBool("Run", false);
+            }
+
+            if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+            {
+                rigidbody.AddForce(Vector2.up * force);
+                animator.SetBool("Jump", true);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision != null)
+        {
+            if(collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = true;
+                animator.SetBool("Jump", false);
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = false;
+            }
         }
     }
 }
